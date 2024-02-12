1556d91cfd7404860a39a152eec4ec6466915e07
welton-2020 <weltdias@gmail.com>
2024-02-12 20:15:19 -0300

    Removendo LAZY da entidade User

--- src/main/java/com/iamspe/sismedicine/entity/User.java
+++ src/main/java/com/iamspe/sismedicine/entity/User.java
@@ -33,7 +33,7 @@ public class User {
 	@Column(nullable = false, unique = true)
 	private String matricula;
 	
-	@OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+	@OneToMany( cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
 	private List<Phone> phones;
 
 	public Long getId() {
